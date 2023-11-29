fdc4c5043edba6251f2201aac02a1a8b51dcdc35
pradeepa1coder <pradeepyadavgnp@gmail.com>
2023-11-29 15:58:20 +0530

    Flames_cal

---
+++ FlamesCalculator/src/main/java/org/jsp/dto/User.java
@@ -0,0 +1,25 @@
+package org.jsp.dto;
+
+public class User {
+
+	private String fname;
+
+	public String getFname() {
+		return fname;
+	}
+
+	public void setFname(String fname) {
+		this.fname = fname;
+	}
+
+	public String getSname() {
+		return sname;
+	}
+
+	public void setSname(String sname) {
+		this.sname = sname;
+	}
+
+	private String sname;
+
+}
